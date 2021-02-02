fd0e6a38301e3f5128d706c361190eff2a1d1315
digriz2021 <77649654+digriz2021@users.noreply.github.com>
2021-02-01 18:59:28 -0600

    Update playing-craps.py

--- playing-craps.py
+++ playing-craps.py
@@ -1,1 +1,42 @@
+# fig04_01.py
+"""Roll two six-sided die 6,000,000 times."""
+import random
 
+# face frequency counters
+frequency1 = 0
+frequency2 = 0
+frequency3 = 0
+frequency4 = 0
+frequency5 = 0
+frequency6 = 0
+
+# 6,000,000 die rolls
+for roll in range(6_000_000):  # note underscore seperators
+    face = random.randrange(1, 7) + random.randrange(1, 7)
+    
+    # increment appropriate face counter
+    if face == 1:
+        frequency1 += 1
+    elif face == 2:
+        frequency2 += 1
+    elif face == 3:
+        frequency3 += 1
+    elif face == 4:
+        frequency4 += 1
+    elif face == 5:
+        frequency5 += 1
+    elif face == 6:
+        frequency6 += 1
+    elif face == 7:
+        frequency7 += 1
+    elif face == 8:
+        frequency8 += 1    
+    elif face == 9:
+        frequency9 += 1
+    elif face == 10:
+        frequency10 += 1
+    elif face == 11:
+        frequency11 += 1
+    elif face == 12:
+        frequency12 += 1
+        
